f941df7ddfae3aaa1f1e8b32b23eaeb86455c977
Christoffer Bredo Lillelund <christofferbredo@gmail.com>
2022-12-05 21:39:55 +0100

    Begin on Home component and add fonts and styles

--- bryllupsite/src/app/app-routing.module.ts
+++ bryllupsite/src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
 
 const routes: Routes = [
-  { path: '', component: AppComponent},
+  { path: '', component: HomeComponent},
 
   //Wild Card Route for 404 request
   { path: '**', pathMatch: 'full', 
--- bryllupsite/src/app/app.component.ts
+++ bryllupsite/src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as WebFont from 'webfontloader';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'bryllupsite';
+
+export class AppComponent implements OnInit {
+  title = 'Wessman & Lillelund';
+
+  ngOnInit(): void {
+    WebFont.load({
+      google: {
+        families: ['Playfair Display']
+      }
+    })
+  }
 }
