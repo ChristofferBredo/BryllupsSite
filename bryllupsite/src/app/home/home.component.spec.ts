747cb47a9fafb259a8a05f725218b57f9f279193
Christoffer Bredo Lillelund <christofferbredo@gmail.com>
2022-12-08 21:21:44 +0100

    Fix missing component in unit test

--- bryllupsite/src/app/home/home.component.spec.ts
+++ bryllupsite/src/app/home/home.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { AgendaComponent } from '../components/agenda/agenda.component';
 
 import { HomeComponent } from './home.component';
 
@@ -8,7 +9,10 @@ describe('HomeComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
+      declarations: [ 
+        HomeComponent,
+        AgendaComponent
+      ]
     })
     .compileComponents();
 
