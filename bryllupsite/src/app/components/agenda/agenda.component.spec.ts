97c23bd8d2944c0a579bf6df1e1662890bb37a05
MedieManden <christofferBredo@gmail.com>
2023-05-28 20:01:00 +0200

    Fix unit tests

--- bryllupsite/src/app/components/agenda/agenda.component.spec.ts
+++ bryllupsite/src/app/components/agenda/agenda.component.spec.ts
@@ -3,6 +3,9 @@ import { MockComponent } from 'ng-mocks';
 import { AgendaItemComponent } from '../agenda-item/agenda-item.component';
 
 import { AgendaComponent } from './agenda.component';
+import { MatCard, MatCardHeader } from '@angular/material/card';
+import { MatDivider } from '@angular/material/divider';
+import { MatList, MatListItem } from '@angular/material/list';
 
 describe('AgendaComponent', () => {
   let component: AgendaComponent;
@@ -12,7 +15,12 @@ describe('AgendaComponent', () => {
     await TestBed.configureTestingModule({
       declarations: [ 
         AgendaComponent, 
-        MockComponent(AgendaItemComponent)  
+        MockComponent(AgendaItemComponent),
+        MatCard,
+        MatCardHeader,
+        MatDivider,
+        MatList,
+        MatListItem
       ]
     })
     .compileComponents();
