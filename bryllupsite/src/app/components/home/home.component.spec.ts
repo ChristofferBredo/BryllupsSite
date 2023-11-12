06d582804eecd1314740544d3f2ac9d85e8539b6
Christoffer Bredo Lillelund <ChristofferBredo@gmail.com>
2023-11-12 20:56:51 +0100

    Remove deprecated component

--- bryllupsite/src/app/components/home/home.component.spec.ts
+++ bryllupsite/src/app/components/home/home.component.spec.ts
@@ -4,7 +4,6 @@ import { MockComponent } from 'ng-mocks';
 
 import { HomeComponent } from './home.component';
 import { LocationComponent } from '../location/location.component';
-import { HomeItemComponent } from './home-item/home-item.component';
 import { MatCard, MatCardContent, MatCardTitle } from '@angular/material/card';
 import { MatGridList, MatGridTile } from '@angular/material/grid-list';
 import { MatList, MatListItem } from '@angular/material/list';
@@ -18,7 +17,6 @@ describe('HomeComponent', () => {
     await TestBed.configureTestingModule({
       declarations: [
         HomeComponent,
-        MockComponent(HomeItemComponent),
         MatCard,
         MatCardContent,
         MatGridList,
