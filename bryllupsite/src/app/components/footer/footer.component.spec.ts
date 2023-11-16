d66ae1cea2487a0dcd19f462639ed0adfbcf5b88
Christoffer Bredo Lillelund <ChristofferBredo@gmail.com>
2023-11-16 21:44:25 +0100

    Fix unit tests

--- bryllupsite/src/app/components/footer/footer.component.spec.ts
+++ bryllupsite/src/app/components/footer/footer.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FooterComponent } from './footer.component';
+import { MatDivider } from '@angular/material/divider';
+import { MatGridList, MatGridTile } from '@angular/material/grid-list';
 
 describe('FooterComponent', () => {
   let component: FooterComponent;
@@ -8,7 +10,12 @@ describe('FooterComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [FooterComponent]
+      declarations: [
+        FooterComponent,
+        MatDivider,
+        MatGridList,
+        MatGridTile
+      ]
     });
     fixture = TestBed.createComponent(FooterComponent);
     component = fixture.componentInstance;
